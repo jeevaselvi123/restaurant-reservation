c358c3b62a266dba5dc5ddfc61b836b9d27fcf7a
Jeeva Dharani Kamaraj <46011656+jeevaselvi123@users.noreply.github.com>
2023-05-12 19:51:56 +0530

    Add files via upload

--- components/ResturantHeader.tsx
+++ components/ResturantHeader.tsx
@@ -1,9 +1,18 @@
-export default function RestaurantHeader() {
+interface Props {
+  restaurantName: string;
+}
+
+export default function RestaurantHeader({ restaurantName }: Props) {
+  const renderTitle = () => {
+    const nameArray = restaurantName.split("-");
+    nameArray[nameArray.length - 1] = `(${nameArray[nameArray.length - 1]})`;
+    return nameArray.join(" ");
+  };
   return (
     <div className="h-96 overflow-hidden">
       <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-        <h1 className="text-7xl text-white captitalize text-shadow text-center">
-          Milestones Grill (Toronto)
+        <h1 className=" capitalize text-7xl text-white text-shadow text-center">
+          {renderTitle()}
         </h1>
       </div>
     </div>
--- components/SearchRestaurantCard.tsx
+++ components/SearchRestaurantCard.tsx
@@ -1,28 +1,34 @@
+import { Cuisine, Location, PRICE } from "@prisma/client";
 import Link from "next/link";
+import Price from "./Price";
 
-export default function SearchRestaurantCard() {
+interface Restaurant {
+  name: string;
+  main_image: string;
+  price: PRICE;
+  slug: string;
+  cusine: Cuisine;
+  location: Location;
+}
+export default function SearchRestaurantCard({ item }: { item: Restaurant }) {
   return (
-    <div className="border-b flex pb-5">
-      <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-        alt=""
-        className="w-44 rounded"
-      />
+    <div className="border-b flex pb-5 ml-4">
+      <img src={item.main_image} alt="" className="w-44 h-36 rounded" />
       <div className="pl-5">
-        <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+        <h2 className="text-3xl">{item.name}</h2>
         <div className="flex items-start">
           <div className="flex mb-2">*****</div>
           <p className="ml-2 text-sm">Awesome</p>
         </div>
         <div className="mb-9">
           <div className="font-light flex text-reg">
-            <p className="mr-4">$$$</p>
-            <p className="mr-4">Mexican</p>
-            <p className="mr-4">Ottawa</p>
+            <Price price={item.price} />
+            <p className="mr-4 capitalize">{item.cusine.name}</p>
+            <p className="mr-4 capitalize">{item.location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
-          <Link href="/restaurant/milestone-grill">View more information</Link>
+          <Link href={`/restaurant/${item.slug}`}>View more information</Link>
         </div>
       </div>
     </div>
